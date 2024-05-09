9cbf21d932b9b47828c35787c414b1d96c088769
Flame260 <162753205+Flame260@users.noreply.github.com>
2024-05-09 11:39:40 -0400

    Revert "fix error"
    This reverts commit cef6c3d3f42cbf86a074133a15f3ee80e16ea130.

--- My-project/Assets/scripts/hello_world.cs
+++ My-project/Assets/scripts/hello_world.cs
@@ -7,7 +7,7 @@ public class hello_world : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("hello world!");
+       debug.Log("hello world!") 
     }
 
     // Update is called once per frame
